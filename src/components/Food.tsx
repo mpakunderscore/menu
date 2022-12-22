0b40303c3d8efb6965f828a514d3412624841de8
mpakunderscore <mpakunderscore@gmail.com>
2022-12-22 19:59:52 +0400

    Fix titles

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -1,13 +1,17 @@
 import React, {useState, useEffect, useReducer, useRef} from "react";
+import {items} from "./Items";
 
 const Food = () => {
 
-    let food = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    let food = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
 
     return (
         <div id={'food'}>
-            {food.map(dish => {
-                return (<img key={dish} src={'./images/food/' + dish + '.png'}/>)
+            {food.map((name, i) => {
+                return <div>
+                    <div>{items[i]}</div>
+                    <img key={i} title={items[i]} src={'./images/food/' + name + '.png'}/>
+                </div>
             })}
         </div>
 
